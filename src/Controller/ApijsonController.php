cf6b81b157c1f9083eb28c55099ca6aabb2fa1ca
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-06-18 01:25:36 +0200

    display database json

--- src/Controller/ApijsonController.php
+++ src/Controller/ApijsonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,12 @@ class ApijsonController extends AbstractController
      */
     public function index(): Response
     {
+
+        $repo = $this->getDoctrine()->getRepository(Annonce::class);
+        $annonce = $repo->findAll();
+
         return $this->json([
-            'message'=>'Welcome to json api',
-            'path'=> 'ici'
+            $annonce
         ]);
 
     }
