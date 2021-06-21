128195fc5a8ddda510616b06b2f75431ddfef4f1
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-06-22 00:23:28 +0200

    api display json

--- src/Controller/ApijsonController.php
+++ src/Controller/ApijsonController.php
@@ -3,24 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Annonce;
+use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApijsonController extends AbstractController
 {
     /**
-     * @Route("/json", name="/json")
+     * @Route("/json", name="/json", methods={"GET"})
+     * @param AnnonceRepository $annonceRepository
+     * @param NormalizerInterface $normalizer
+     * @return Response
+     * @throws ExceptionInterface
      */
-    public function index(): Response
+    public function index(AnnonceRepository $annonceRepository, SerializerInterface $serializer): Response
     {
+        $annonce = $annonceRepository->findAll();
 
-        $repo = $this->getDoctrine()->getRepository(Annonce::class);
-        $annonce = $repo->findAll();
+       $json = $serializer->serialize($annonce, 'json');
 
-        return $this->json([
-            $annonce
+        $response = new Response($json, 200, [
+           "Content-Type" => "application/json"
         ]);
 
+        return $response;
     }
 }
